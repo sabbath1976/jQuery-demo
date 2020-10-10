b55c88fb6d133bf3d5c23554c89af9a16e1043ee
Thomas <sabbath1976@gmail.com>
2020-10-10 08:46:49 +0300

    jQuery - Events
    Event Object

--- js/script.js
+++ js/script.js
@@ -96,31 +96,63 @@
 // });
 
 // When we click the first object, the background gets red
-$(function () {
-    $(".box-1").on({
-        click: function () {
-            $("body").css({
-                background: "orange",
-            });
-        },
-    });
+// $(function () {
+//     $(".box-1").on({
+//         click: function () {
+//             $("body").css({
+//                 background: "orange",
+//             });
+//         },
+//     });
 
     // When we click the second object, the background gets purple,
-    $(".box-2").on({
-        click: function () {
-            $("body").css({
-                background: "purple",
-            });
-        },
-        mouseenter: function () {
-            $("body").css({
-                background: "yellow",
-            });
-        },
-        mouseleave: function () {
-            $("body").css({
-                background: "red",
-            });
-        },
-    });
-});
+//     $(".box-2").on({
+//         click: function () {
+//             $("body").css({
+//                 background: "purple",
+//             });
+//         },
+//         mouseenter: function () {
+//             $("body").css({
+//                 background: "yellow",
+//             });
+//         },
+//         mouseleave: function () {
+//             $("body").css({
+//                 background: "red",
+//             });
+//         },
+    // });
+// });
+
+$(function() {
+    $('.box-1 a').on('click', function(event) {
+        event.preventDefault()
+        $('.box-3').css({
+            background: 'purple'
+        })
+    })
+
+    $('.box-2').on('click', function() {
+        $('.box-3').css({
+            background: 'red'
+        })
+    })
+
+    $('.button').on('click', function(event) {
+        event.stopPropagation()
+        
+        if (event.target.nodeName == 'DIV') {
+            console.log(event.target.nodeName);
+            $('.box-3').css({
+                background: 'purple'
+            })
+        } else {
+            console.log(event.target.nodeName)
+            $('.box-3').css({
+                background: 'yellow'
+            })
+        }
+        
+    })
+})
